02d9e6e3ba14df0fc8249f3451f8c349e18dbebf
Pamellino <Pamellino@users.noreply.github.com>
2018-03-10 19:16:31 +0100

    c

--- medag_hackaton/medag_hackaton/medag_hackaton/Pages/RegisterPage.xaml.cs
+++ medag_hackaton/medag_hackaton/medag_hackaton/Pages/RegisterPage.xaml.cs
@@ -16,10 +16,6 @@ namespace medag_hackaton.Pages
 	{
 		public RegisterPage ()
 		{
-<<<<<<< HEAD
-            BindingContext = new LoginRegisterViewModel(new Navigations());
-=======
->>>>>>> 92c85f2d1ee3e3b4169af887927f1887743d398d
             InitializeComponent ();
             BindingContext = new LoginRegisterViewModel(new Navigations());
 
