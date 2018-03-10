d630082fa0916aef5c43f65689690aeb87412090
gaspard dv <gaspard.villenfagne@gmail.com>
2018-03-10 19:31:21 +0100

    azerty

--- medag_hackaton/medag_hackaton/medag_hackaton/App.xaml.cs
+++ medag_hackaton/medag_hackaton/medag_hackaton/App.xaml.cs
@@ -14,7 +14,7 @@ namespace medag_hackaton
 		{
 			InitializeComponent();
 
-			MainPage = new NavigationPage( new RegisterPage() );
+			MainPage = new NavigationPage( new HomePage());
 		}
 
 		protected override void OnStart ()
