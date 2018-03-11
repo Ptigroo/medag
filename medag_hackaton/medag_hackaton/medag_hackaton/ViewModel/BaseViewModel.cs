a9fe4e206c939da83f7142244aa40e5f74cc89be
Walfhand <vandaele.seba@gmail.com>
2018-03-11 11:07:18 +0100

    gfgfgfhg

--- medag_hackaton/medag_hackaton/medag_hackaton/ViewModel/BaseViewModel.cs
+++ medag_hackaton/medag_hackaton/medag_hackaton/ViewModel/BaseViewModel.cs
@@ -4,19 +4,28 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
-
+using System.Threading;
 
 namespace medag_hackaton.ViewModel
 {
     class BaseViewModel : INotifyPropertyChanged
     {
+        private SynchronizationContext synchronizationContext = SynchronizationContext.Current;
         protected readonly INavigation navigation;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected void OnPropertyChanged([CallerMemberName] string name = null)
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            if(synchronizationContext != null)
+            {
+                synchronizationContext.Post((s) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name)),null);
+            }
+            else
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            }
+            
         }
 
         public BaseViewModel(INavigation navigation)
